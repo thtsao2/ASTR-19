10d7de87ad8ee0cc1b263f5431ff2fc4a6786841
thtsao2 <thtsao@ucsc.edu>
2026-04-06 16:25:18 -0700

    Session 1 Prompt

---
+++ Session 1 Prompt.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:17:26 2026
+
+@author: thomas
+"""
+
+def Introduction():
+    print("My full name is Thomas Tsao")
+    print("My favorite movie is Cars, because I really enjoyed it growing up.")
+    print("My favorite food is probably sushi, with especially super fresh fish.")
+
+def Main():
+    Introduction()
+    
+if __name__ == "__main__":
+    Main()
+    
+    
